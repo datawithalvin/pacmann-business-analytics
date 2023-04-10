aa7707c5084e4f976ffff691bbbda669d63272c0
Data with Alvin <alvinnoza.data@gmail.com>
2023-04-10 20:48:33 +0700

    add new components

--- scripts/main.py
+++ scripts/main.py
@@ -8,9 +8,10 @@ from dash import dcc, html
 from dash.dependencies import Input, Output
 
 import plotly.express as px
-import plotly.graph_objects as go
+import plotly.graph_objs as go
+from plotly.subplots import make_subplots
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY, "./style.css"])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY, "./style.css", "./dropdown.css"])
 
 choropleth_map_urls = {
     2015: "https://datawrapper.dwcdn.net/HDZHt/1/",
@@ -49,25 +50,38 @@ content = dbc.Col(
         dbc.Row(
             [
                 dbc.Col(html.Iframe(id="choropleth_map", src=choropleth_map_urls[2017], style={"width": "100%", "height": "53vh", "border": "none"}), width={"size": 7, "order": 1, "offset": 0}),
-                dbc.Col(dcc.Graph(id="barfig1", figure={}, className="chart-container"), width={"size": 3, "order": 2, "offset": 0}),
+                dbc.Col(
+                    [
+                        html.P("Filter by Region"),
+                        html.Div(
+                            [
+                                dcc.Dropdown(
+                                    id="region_filter",
+                                    options=[
+                                        {"label": region, "value": region} for region in main_df["order_region"].unique()
+                                    ],
+                                    value="Southern Asia",
+                                    clearable=False,
+                                    searchable=False,
+                                    className="small-dropdown-child",
+                                ),
+                            ],
+                            className="small-dropdown-wrapper",
+                        ),
+                        dcc.Graph(id="productbar", figure={}, className="chart-container")
+                    ],
+                    width={"size": 3, "order": 2, "offset": 0}
+                ),
             ],
         ),
 
         dbc.Row(
             [
-                dbc.Col(dcc.Graph(id="barfig2", figure={}, className="chart-container"), style={"height": "40vh", "border": "none"}, width={"size": 3, "order": 1, "offset": 0}),
-                dbc.Col(dcc.Graph(id="barfig3", figure={}, className="chart-container"), width={"size": 4, "order": 2, "offset": 0}),
+                dbc.Col(dcc.Graph(id="topbarfig", figure={}, className="chart-container"), style={"height": "40vh", "border": "none"}, width={"size": 3, "order": 1, "offset": 0}),
+                dbc.Col(dcc.Graph(id="bottombarfig", figure={}, className="chart-container"), width={"size": 4, "order": 2, "offset": 0}),
                 dbc.Col(dcc.Graph(id="fill_rate_line_chart", figure={}, className="chart-container"), width={"size": 3, "order": 3, "offset": 0}),
             ],
         ),
-
-        # dbc.Row(
-        #     [
-        #         dbc.Col(dcc.Graph(id="otif_bar_chart", figure={}, className="chart-container"), width={"size": 3, "order": 1, "offset": 0}),
-        #         dbc.Col(dcc.Graph(id="stacked_bar_chart", figure={}, className="chart-container"), width={"size": 4, "order": 2, "offset": 0}),
-        #         dbc.Col(dcc.Graph(id="heatmap", figure={}, className="chart-container"), width={"size": 3, "order": 3, "offset": 0}),
-        #     ],
-        # ),
     ],
     width={"size": 10, "order": 2, "offset": 0},
     className="h-100",
@@ -88,16 +102,21 @@ app.layout = dbc.Container(
 
 @app.callback(
     Output("choropleth_map", "src"),
-    Output("barfig1", "figure"),
-    Input("year_filter", "value")
+    Output("topbarfig", "figure"),
+    Output("bottombarfig", "figure"),
+    Output("productbar", "figure"),
+    Input("year_filter", "value"),
+    Input("region_filter", "value")
 )
-def update_choropleth_map(year):
+def update_choropleth_map(year, region):
     filtered_df = main_df[main_df["order_year"]==year]
 
-    def create_placeholder_figures(filtered_df):
-        barfig1 = create_bar_region(filtered_df)
+    def create_placeholder_figures(filtered_df, region):
+        topbarfig = create_bar_region(filtered_df)
+        bottombarfig = create_bar_region2(filtered_df)
+        productbar = create_bar_region_combined(filtered_df, region)
 
-        return barfig1
+        return topbarfig, bottombarfig, productbar
     
     def create_bar_region(dataframe):
         grouped = dataframe.groupby(["market", "order_region"]).agg(
@@ -106,29 +125,168 @@ def update_choropleth_map(year):
 
         grouped = grouped.sort_values(by="total_sales", ascending=False).reset_index()
         grouped["total_sales"] = round(grouped["total_sales"], 2)
-        grouped = grouped.head(10)
+        grouped = grouped.head(5)
 
         # format values in millions
         grouped['total_sales_formatted'] = (grouped['total_sales'] / 1000000).round(2).astype(str) + 'M'
 
+        # assign unique colors to each market
+        color_map = {'LATAM': '#3366CC', 'Europe': '#DC3912', 'Pacific Asia': '#FF9900', 'USCA': '#109618', 'Africa': '#990099'}
+
         fig = px.bar(grouped, x="total_sales", y="order_region", orientation="h", text="total_sales_formatted", color="market",
                         labels={"order_region":"", "total_sales":"Total Sales"}, template="plotly_dark",
-                        color_discrete_sequence=px.colors.qualitative.Safe)
+                        color_discrete_map=color_map)
 
         fig.update_layout(yaxis={'categoryorder':'total ascending'})
 
-        fig.update_layout(autosize=True,width=600,height=500)
-        fig.update_layout(title="<b>Top 10 Total Sales Across Different Regions and Markets</b>",title_font_size=14)
+        fig.update_layout(autosize=True,width=400,height=300)
+        fig.update_layout(title="<b>Top 5 High-Performing Regions by Total Sales</b>",title_font_size=13)
         fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)','paper_bgcolor': 'rgba(0, 0, 0, 0)',})
         fig.update_xaxes(title_font=dict(size=12))
         fig.update_layout(xaxis_showgrid=False, yaxis_showgrid=False)
+        fig.update_layout(legend_title_text='Market')
         
 
         return fig
+    
+    def create_bar_region2(dataframe):
+        grouped = dataframe.groupby(["market", "order_region"]).agg(
+            total_sales = ("sales", "sum")
+        ).reset_index()
 
-    barfig1 = create_placeholder_figures(filtered_df)
+        grouped = grouped.sort_values(by="total_sales", ascending=False).reset_index()
+        grouped["total_sales"] = round(grouped["total_sales"], 2)
+        grouped = grouped.tail(5)
 
-    return choropleth_map_urls.get(year), barfig1
+        # format values in millions
+        grouped['total_sales_formatted'] = (grouped['total_sales'] / 1000).round(2).astype(str) + 'K'
+
+        # assign unique colors to each market
+        color_map = {'LATAM': '#3366CC', 'Europe': '#DC3912', 'Pacific Asia': '#FF9900', 'USCA': '#109618', 'Africa': '#990099'}
+
+        fig = px.bar(grouped, x="total_sales", y="order_region", orientation="h", text="total_sales_formatted", color="market",
+                        labels={"order_region":"", "total_sales":"Total Sales"}, template="plotly_dark",
+                        color_discrete_map=color_map)
+
+        fig.update_layout(yaxis={'categoryorder':'total ascending'})
+
+        fig.update_layout(autosize=True,width=400,height=300)
+        fig.update_layout(title="<b>Bottom 5 Underperforming Regions by Total Sales</b>",title_font_size=13)
+        fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)','paper_bgcolor': 'rgba(0, 0, 0, 0)',})
+        fig.update_xaxes(title_font=dict(size=12))
+        fig.update_layout(xaxis_showgrid=False, yaxis_showgrid=False)
+        fig.update_layout(legend_title_text='Market')
+        
+
+        return fig
+    
+    def create_bar_region_combined(dataframe, region):
+        dataframe = dataframe[dataframe["order_region"]==region]
+        grouped = dataframe.groupby(["category_name"]).agg(
+            total_sales=("sales", "sum")
+        ).reset_index()
+
+        grouped["total_sales"] = round(grouped["total_sales"], 2)
+
+        # sort by total_sales and split into two groups
+        grouped = grouped.sort_values(by="total_sales", ascending=False).reset_index()
+        top_5 = grouped.head(5)
+        bottom_5 = grouped.tail(5)
+
+        # create a dictionary to map each category to a unique color
+        category_colors = {
+            'Camping & Hiking': '#1f77b4',
+            'Water Sports': '#ff7f0e',
+            "Women's Apparel": '#2ca02c',
+            "Men's Footwear": '#d62728',
+            'Indoor/Outdoor Games': '#9467bd',
+            'Accessories': '#8c564b',
+            'Cleats': '#e377c2',
+            'Trade-In': '#7f7f7f',
+            'Cardio Equipment': '#bcbd22',
+            'Shop By Sport': '#17becf',
+            'Hockey': '#ff5733',
+            'Electronics': '#e74c3c',
+            'Fishing': '#3498db',
+            'Golf Balls': '#9b59b6',
+            'Lacrosse': '#e67e22',
+            'Baseball & Softball': '#34495e',
+            'Golf Gloves': '#f1c40f',
+            "Girls' Apparel": '#2ecc71',
+            'Fitness Accessories': '#1abc9c',
+            'Hunting & Shooting': '#95a5a6',
+            'Tennis & Racquet': '#2c3e50',
+            'Golf Shoes': '#bdc3c7',
+            'Golf Apparel': '#d35400',
+            'Boxing & MMA': '#7f8c8d',
+            "Men's Golf Clubs": '#2980b9',
+            "Kids' Golf Clubs": '#16a085',
+            'Soccer': '#c0392b',
+            "Women's Golf Clubs": '#f39c12',
+            'Golf Bags & Carts': '#27ae60',
+            'Strength Training': '#e67e22',
+            'As Seen on  TV!': '#8e44ad',
+            'Basketball': '#f39c12',
+            'Books ': '#1abc9c',
+            'Baby ': '#95a5a6',
+            'CDs ': '#d35400',
+            'Cameras ': '#bdc3c7',
+            "Children's Clothing": '#9b59b6',
+            'Computers': '#7f8c8d',
+            'Consumer Electronics': '#2c3e50',
+            'Crafts': '#27ae60',
+            'DVDs': '#f1c40f',
+            'Garden': '#17becf',
+            'Health and Beauty': '#bcbd22',
+            "Men's Clothing": '#e74c3c',
+            'Music': '#8c564b',
+            'Pet Supplies': '#2ecc71',
+            'Sporting Goods': '#7f7f7f',
+            'Toys': '#d62728',
+            'Video Games': '#9467bd',
+            "Women's Clothing": '#3498db'
+        }
+
+        # map category colors to the top_5 and bottom_5 dataframes
+        top_5['color'] = top_5['category_name'].map(category_colors)
+        bottom_5['color'] = bottom_5['category_name'].map(category_colors)
+
+        # create the subplots
+        fig = make_subplots(rows=2, cols=1, vertical_spacing=0.2, subplot_titles=(
+            f"<b>Top 5 High-Performing Categories in {region}</b>", 
+            f"<b>Bottom 5 Underperforming Categories in {region}</b>"))
+
+        # add the top 5 subplot
+        fig.add_trace(go.Bar(x=top_5['total_sales'], y=top_5['category_name'], orientation='h',
+                            text=top_5['total_sales'], name='', marker=dict(color=top_5['color'])),
+                    row=1, col=1)
+        fig.update_yaxes(title='', categoryorder='total ascending', row=1, col=1)
+        fig.update_xaxes(title='Total Sales', row=1, col=1, showgrid=False)
+
+        # add the bottom 5 subplot
+        fig.add_trace(go.Bar(x=bottom_5['total_sales'], y=bottom_5['category_name'], orientation='h',
+                            text=bottom_5['total_sales'], name='', marker=dict(color=bottom_5['color'])),
+                    row=2, col=1)
+        fig.update_yaxes(title='', categoryorder='total ascending', row=2, col=1)
+        fig.update_xaxes(title='Total Sales', row=2, col=1, showgrid=False)
+
+        # update the layout
+        fig.update_layout(
+            height=400,
+            width=600,
+            template="plotly_dark",
+            showlegend=False,
+            margin=dict(l=0, r=0, t=20, b=0),
+        )
+        fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)','paper_bgcolor': 'rgba(0, 0, 0, 0)',})
+        fig.update_annotations(font_size=12)
+
+        return fig
+
+    topbarfig, bottombarfig, productbar = create_placeholder_figures(filtered_df, region)
+
+
+    return choropleth_map_urls.get(year), topbarfig, bottombarfig, productbar
 
 
 
