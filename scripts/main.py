85269de1c78b3bf2f4db80a6d62dd1aab6afb378
Data with Alvin <alvinnoza.data@gmail.com>
2023-04-11 19:06:34 +0700

    update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import time
+import locale
 
 import dash
 import dash_bootstrap_components as dbc
@@ -28,9 +29,9 @@ main_df = pd.read_csv(filepath)
 
 sidebar = dbc.Col(
     [
-        html.H2("Dashboard Description"),
-        html.P("Here you can write a brief description of the dashboard and its purpose."),
-        html.H4("Filter by Year"),
+        html.H3("DataCo Regional Performance Dashboard"),
+        html.Hr(),
+        html.P("Select Year:"),
         dcc.Dropdown(
             id="year_filter",
             options=[
@@ -40,38 +41,42 @@ sidebar = dbc.Col(
             clearable=False,
             searchable=False,
         ),
+        html.Hr(),
+        html.P("Select Region:"),
+        dcc.Dropdown(
+            id="region_filter",
+            options=[
+                {"label": region, "value": region} for region in main_df["order_region"].unique()
+            ],
+            value="Southern Asia",
+            clearable=False,
+            searchable=False,
+        ),
+        html.Hr(),
+        html.P("Here you can write a brief description of the dashboard and its purpose."),
     ],
     width={"size": 2, "order": 1, "offset": 0},
     className="h-100",
 )
 
+
 content = dbc.Col(
     [
+    
         dbc.Row(
             [
-                dbc.Col(html.Iframe(id="choropleth_map", src=choropleth_map_urls[2017], style={"width": "100%", "height": "53vh", "border": "none"}), width={"size": 7, "order": 1, "offset": 0}),
-                dbc.Col(
-                    [
-                        html.P("Filter by Region"),
-                        html.Div(
-                            [
-                                dcc.Dropdown(
-                                    id="region_filter",
-                                    options=[
-                                        {"label": region, "value": region} for region in main_df["order_region"].unique()
-                                    ],
-                                    value="Southern Asia",
-                                    clearable=False,
-                                    searchable=False,
-                                    className="small-dropdown-child",
-                                ),
-                            ],
-                            className="small-dropdown-wrapper",
-                        ),
-                        dcc.Graph(id="productbar", figure={}, className="chart-container")
-                    ],
-                    width={"size": 3, "order": 2, "offset": 0}
-                ),
+                dbc.Col(html.Div([html.H6(id="region_name"), html.H6(id="otif_rate")], style={"width": "100%", "border-style": "double", "text-align": "center"}), width={"size": 2, "order": 5, "offset": 0}),
+                dbc.Col(html.Div([html.H6(id="avg_tittle"), html.H6(id="avg_days")], style={"width": "100%", "border-style": "double", "text-align": "center"}), width={"size": 2, "order": 4, "offset": 0}),
+                dbc.Col(html.Div([html.H6(id="total_tittle"), html.H6(id="total_order")], style={"width": "100%", "border-style": "double", "text-align": "center"}), width={"size": 2, "order": 1, "offset": 0}),
+                dbc.Col(html.Div([html.H6(id="sales_tittle"), html.H6(id="total_sales")], style={"width": "100%", "border-style": "double", "text-align": "center"}), width={"size": 2, "order": 2, "offset": 0}),
+                dbc.Col(html.Div([html.H6(id="profit_tittle"), html.H6(id="total_profit")], style={"width": "100%", "border-style": "double", "text-align": "center"}), width={"size": 2, "order": 3, "offset": 0}),
+            ],
+        ),
+
+        dbc.Row(
+            [
+                dbc.Col(html.Iframe(id="choropleth_map", src=choropleth_map_urls[2017], style={"width": "100%", "height": "40vh", "border": "none"}), width={"size": 5, "order": 2, "offset": 0}),
+                dbc.Col(dcc.Graph(id="productbar", figure={}, className="chart-container"), width={"size": 5, "order": 1, "offset": 0}),
             ],
         ),
 
@@ -105,18 +110,39 @@ app.layout = dbc.Container(
     Output("topbarfig", "figure"),
     Output("bottombarfig", "figure"),
     Output("productbar", "figure"),
+    Output("otif_rate", "children"),
+    Output("region_name", "children"),
+    Output("avg_days", "children"),
+    Output("avg_tittle", "children"),
+    Output("total_tittle", "children"),
+    Output("total_order", "children"),
+    Output("sales_tittle", "children"),
+    Output("total_sales", "children"),
+    Output("profit_tittle", "children"),
+    Output("total_profit", "children"),   
     Input("year_filter", "value"),
     Input("region_filter", "value")
 )
-def update_choropleth_map(year, region):
+def update_dashboard(year, region):
     filtered_df = main_df[main_df["order_year"]==year]
 
     def create_placeholder_figures(filtered_df, region):
         topbarfig = create_bar_region(filtered_df)
         bottombarfig = create_bar_region2(filtered_df)
         productbar = create_bar_region_combined(filtered_df, region)
+        otif_rate = calculate_otif(filtered_df, region)
+        region_name = "OTIF Rate"
+        avg_days = calculate_avg_shipping(filtered_df, region)
+        avg_tittle = "Average Delivered Days"
+        total_tittle = "Total Order"
+        total_order = calculate_total_order(filtered_df, region)
+        sales_tittle = "Total Sales"
+        total_sales = calculate_total_sales(filtered_df, region)
+        profit_tittle = "Total Profit"
+        total_profit = calculate_total_profit(filtered_df, region)
 
-        return topbarfig, bottombarfig, productbar
+
+        return topbarfig, bottombarfig, productbar, otif_rate, region_name, avg_days, avg_tittle, total_tittle, total_order, sales_tittle, total_sales, profit_tittle, total_profit
     
     def create_bar_region(dataframe):
         grouped = dataframe.groupby(["market", "order_region"]).agg(
@@ -125,15 +151,16 @@ def update_choropleth_map(year, region):
 
         grouped = grouped.sort_values(by="total_sales", ascending=False).reset_index()
         grouped["total_sales"] = round(grouped["total_sales"], 2)
+        
         grouped = grouped.head(5)
 
         # format values in millions
-        grouped['total_sales_formatted'] = (grouped['total_sales'] / 1000000).round(2).astype(str) + 'M'
+        grouped['total_sales_formated'] = "$" + (grouped['total_sales'] / 1000000).round(2).astype(str) + 'M'
 
         # assign unique colors to each market
         color_map = {'LATAM': '#3366CC', 'Europe': '#DC3912', 'Pacific Asia': '#FF9900', 'USCA': '#109618', 'Africa': '#990099'}
 
-        fig = px.bar(grouped, x="total_sales", y="order_region", orientation="h", text="total_sales_formatted", color="market",
+        fig = px.bar(grouped, x="total_sales", y="order_region", orientation="h", text="total_sales_formated", color="market",
                         labels={"order_region":"", "total_sales":"Total Sales"}, template="plotly_dark",
                         color_discrete_map=color_map)
 
@@ -156,15 +183,16 @@ def update_choropleth_map(year, region):
 
         grouped = grouped.sort_values(by="total_sales", ascending=False).reset_index()
         grouped["total_sales"] = round(grouped["total_sales"], 2)
+
         grouped = grouped.tail(5)
 
         # format values in millions
-        grouped['total_sales_formatted'] = (grouped['total_sales'] / 1000).round(2).astype(str) + 'K'
+        grouped['total_sales_formated'] = "$" + (grouped['total_sales'] / 1000).round(2).astype(str) + 'K'
 
         # assign unique colors to each market
         color_map = {'LATAM': '#3366CC', 'Europe': '#DC3912', 'Pacific Asia': '#FF9900', 'USCA': '#109618', 'Africa': '#990099'}
 
-        fig = px.bar(grouped, x="total_sales", y="order_region", orientation="h", text="total_sales_formatted", color="market",
+        fig = px.bar(grouped, x="total_sales", y="order_region", orientation="h", text="total_sales_formated", color="market",
                         labels={"order_region":"", "total_sales":"Total Sales"}, template="plotly_dark",
                         color_discrete_map=color_map)
 
@@ -190,6 +218,11 @@ def update_choropleth_map(year, region):
 
         # sort by total_sales and split into two groups
         grouped = grouped.sort_values(by="total_sales", ascending=False).reset_index()
+
+        # format total_sales as a string with $ and thousand separator
+        locale.setlocale(locale.LC_ALL, '')  # set locale to default system locale
+        grouped["total_sales_formated"] = grouped["total_sales"].apply(lambda x: locale.currency(x, grouping=True))
+
         top_5 = grouped.head(5)
         bottom_5 = grouped.tail(5)
 
@@ -258,14 +291,14 @@ def update_choropleth_map(year, region):
 
         # add the top 5 subplot
         fig.add_trace(go.Bar(x=top_5['total_sales'], y=top_5['category_name'], orientation='h',
-                            text=top_5['total_sales'], name='', marker=dict(color=top_5['color'])),
+                            text=top_5['total_sales_formated'], name='', marker=dict(color=top_5['color'])),
                     row=1, col=1)
         fig.update_yaxes(title='', categoryorder='total ascending', row=1, col=1)
         fig.update_xaxes(title='Total Sales', row=1, col=1, showgrid=False)
 
         # add the bottom 5 subplot
         fig.add_trace(go.Bar(x=bottom_5['total_sales'], y=bottom_5['category_name'], orientation='h',
-                            text=bottom_5['total_sales'], name='', marker=dict(color=bottom_5['color'])),
+                            text=bottom_5['total_sales_formated'], name='', marker=dict(color=bottom_5['color'])),
                     row=2, col=1)
         fig.update_yaxes(title='', categoryorder='total ascending', row=2, col=1)
         fig.update_xaxes(title='Total Sales', row=2, col=1, showgrid=False)
@@ -273,7 +306,7 @@ def update_choropleth_map(year, region):
         # update the layout
         fig.update_layout(
             height=400,
-            width=600,
+            width=500,
             template="plotly_dark",
             showlegend=False,
             margin=dict(l=0, r=0, t=20, b=0),
@@ -283,10 +316,60 @@ def update_choropleth_map(year, region):
 
         return fig
 
-    topbarfig, bottombarfig, productbar = create_placeholder_figures(filtered_df, region)
+    def calculate_otif(dataframe, region):
+        dataframe['on_time_in_full'] = (dataframe['shipping_days_difference'] >= 0).astype(int)
+        # Group the data by 'order_region' and calculate the total number of orders and the total number of on-time, in-full orders.
+        region_otif_data = dataframe.groupby('order_region')['on_time_in_full'].agg(['sum', 'count']).reset_index()
+
+        # Calculate the OTIF rate for each region by dividing the total number of on-time, in-full orders by the total number of orders, then multiply by 100 to get the percentage.
+        region_otif_data['otif_rate'] = (region_otif_data['sum'] / region_otif_data['count']) * 100
+
+        # Rename the columns for better readability
+        region_otif_data.columns = ['order_region', 'on_time_in_full_orders', 'total_orders', 'otif_rate']
+
+        value = str(round(region_otif_data[region_otif_data["order_region"]==region]["otif_rate"].values[0], 2)) + " %"
+
+        return value
+    
+    def calculate_avg_shipping(dataframe, region):
+
+        avg_scheduled_shipping_time = dataframe.groupby(['order_region'])['days_for_shipping_real'].mean().reset_index()
+        avg_scheduled_shipping_time = str(round(avg_scheduled_shipping_time[avg_scheduled_shipping_time["order_region"]==region]["days_for_shipping_real"].values[0], 2)) + " days"
+
+        return avg_scheduled_shipping_time
+    
+    def calculate_total_order(dataframe, region):
+
+        total_order = dataframe.groupby(['order_region'])['order_item_quantity'].sum().reset_index()
+        order_value = total_order[total_order["order_region"]==region]["order_item_quantity"].values[0]
+        formatted_order = "{:,.0f}".format(order_value)
+        total_order = formatted_order
+
+        return total_order
+    
+    def calculate_total_sales(dataframe, region):
+
+        total_sales = dataframe.groupby(['order_region'])['sales'].sum().reset_index()
+        sales_value = total_sales[total_sales["order_region"]==region]["sales"].values[0]
+        formatted_sales = "${:,.2f}".format(sales_value)
+        total_sales = formatted_sales
+
+        return total_sales
+    
+    def calculate_total_profit(dataframe, region):
+
+        total_profit = dataframe.groupby(['order_region'])['order_profit_per_order'].sum().reset_index()
+        profit_value = total_profit[total_profit["order_region"]==region]["order_profit_per_order"].values[0]
+        formatted_profit = "${:,.2f}".format(profit_value)
+        total_profit = formatted_profit
+
+        return total_profit
 
 
-    return choropleth_map_urls.get(year), topbarfig, bottombarfig, productbar
+    topbarfig, bottombarfig, productbar, otif_rate, region_name, avg_days, avg_tittle, total_tittle, total_order, sales_tittle,  total_sales, profit_tittle, total_profit = create_placeholder_figures(filtered_df, region)
+
+
+    return choropleth_map_urls.get(year), topbarfig, bottombarfig, productbar, otif_rate, region_name, avg_days, avg_tittle, total_tittle, total_order, sales_tittle,  total_sales, profit_tittle, total_profit
 
 
 
